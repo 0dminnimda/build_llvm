9c026fe8a9fc2dc83557ec9eaafbbf900d86e75a
0dminnimda <0dminnimda@gmail.com>
2024-08-12 01:42:48 +0300

    build.sh: let's try compact release [build]

--- build.sh
+++ build.sh
@@ -4,11 +4,13 @@
 set -o errexit
 set -o errtrace
 
+BUILD_TYPE=MinSizeRel # Release # RelWithDebInfo
+
 cd llvm-project
 cmake -S llvm -B build -G Ninja \
     -DCMAKE_C_COMPILER=$(which gcc) \
     -DCMAKE_CXX_COMPILER=$(which g++) \
-    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+    -DCMAKE_BUILD_TYPE=$(BUILD_TYPE) \
     -DLLVM_ENABLE_PROJECTS='clang' \
     -DLLVM_TARGETS_TO_BUILD='X86'
     # -DLLVM_PARALLEL_COMPILE_JOBS=4
